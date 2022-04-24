abdd787bbd4ba089c659a9ce4058b25542a5e7e3
vucan1892000@gmail.com <vucan1892000@gmail.com>
2022-04-24 22:14:30 +0700

    finish part 16 user profile with one to one

--- accounts/decorator.py
+++ accounts/decorator.py
@@ -20,11 +20,10 @@ def allowed_users(allowed_roles=[]):
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
 
-            if group not in allowed_roles:
-                return HttpResponse('You are not authorized to view this page')
-            else:
+            if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
-
+            else:
+                return HttpResponse('You are not authorized to view this page')
         return wrapper_func
     return decorator
 
