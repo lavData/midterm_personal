43e58cb994e1c52623de2f483430bd5f568e9a45
vucan1892000@gmail.com <vucan1892000@gmail.com>
2022-04-24 20:09:36 +0700

    finish part 12 from table search

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
+
 
 def home(request):
     orders = Order.objects.all()
@@ -29,7 +31,15 @@ def customer(request, pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
     order_count = orders.count()
-    context = {'customer': customer, 'orders': orders, 'order_count': order_count}
+
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
+
+    context = {'customer': customer,
+               'orders': orders,
+               'order_count': order_count,
+               'myFilter': myFilter
+               }
     return render(request, 'accounts/customer.html', context)
 
 
