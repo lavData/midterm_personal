be102764a566de0cb643e4bed3b86f36ee7ea77e
vucan1892000@gmail.com <vucan1892000@gmail.com>
2022-04-24 10:33:12 +0700

    finish part 6 db relationship

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Customer, Product, Order
+from .models import *
 
 
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(Order)
+
